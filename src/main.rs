020afbcaef2f0143e6e497425282d166ef2fbaf4
Santo Cariotti <santo@dcariotti.me>
2023-10-15 21:01:02 +0200

    Add other keycodes for scrolling

--- src/main.rs
+++ src/main.rs
@@ -104,20 +104,20 @@ fn handle_events(ui: &mut UI) -> io::Result<bool> {
                     KeyCode::Char('q') => {
                         return Ok(true);
                     }
-                    KeyCode::Char('j') => {
+                    KeyCode::Char('j') | KeyCode::Down => {
                         if ui.scroll < (ui.max_lines - ui.height + 1) {
                             ui.scroll += 1;
                         }
                     }
-                    KeyCode::Char('J') => {
+                    KeyCode::Char('J') | KeyCode::Char('G') => {
                         ui.scroll = ui.max_lines - ui.height + 1;
                     }
-                    KeyCode::Char('k') => {
+                    KeyCode::Char('k') | KeyCode::Up => {
                         if ui.scroll > 1 {
                             ui.scroll -= 1;
                         }
                     }
-                    KeyCode::Char('K') => {
+                    KeyCode::Char('K') | KeyCode::Char('0') => {
                         ui.scroll = ui.height;
                     }
                     _ => {}
